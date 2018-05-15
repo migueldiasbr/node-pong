76b1f178329f68381b675400ab3f5046a597f5b1
Miguel Dias <migueldiasbr@gmail.com>
2018-05-15 16:00:39 -0300

    version 1.2.0

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # node-pong
 
+![node-pong](./node-pong.png)
+
 ## Summary
 
 Every time a developer wants to check if a site/API is up and running, usually a PING is made on a IP and PORT. Sometimes I want to check if my service is up on a determined PORT, or even if my request is going through the firewall/router/switch the way I expect.
@@ -44,6 +46,13 @@ and you should see the server running:
 server running on port 13420
 ```
 
+
+## Main endpoint
+
+### ``/ping``
+
+The main endpoint for all the action on this package is ``/ping``. Every port that is provided to be listened by the server will have this endpoint.
+
 ## Check response
 
 Now you only have to check if the server is responding using any language/program you want. Example:
@@ -56,6 +65,90 @@ and you should see a response (drum roll)
 
 ```bash
 pong
+```
+
+### Other response formats
+
+Since version 1.2.0 it's possible to get the response in other format than plain text.
+
+- HTML
+- JSON
+- XML
+
+#### HTML
+
+```bash
+curl -H "Accept: text/html" http://localhost:13420/ping
+```
+
+```html
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>node-pong response</title>
+</head>
+
+<body>
+    <h1>pong</h1>
+</body>
+
+</html>
+```
+
+#### JSON
+
+```bash
+curl -H "Accept: application/json" http://localhost:13420/ping
+```
+
+```json
+{
+    "headers": {
+        "host": "localhost:13420",
+        "user-agent": "curl/7.54.0",
+        "accept": "application/json"
+    },
+    "method": "GET",
+    "url": "/ping",
+    "body": "{\"answer\": \"pong\"}"
+}
+```
+
+#### XML
+
+```bash
+curl -H "Accept: application/xml" http://localhost:13420/ping
+```
+
+```xml
+<?xml version="1.0" encoding="utf-8" ?>
+<answer>pong</answer>
+```
+
+### Multiple HTTP ACCEPT HEADERS
+
+We want just one kind of response, so **IF** multiple **ACCEPT** headers are found, the **FIRST** one on the following order will be the response:
+
+1. JSON
+2. HTML
+3. XML
+
+In the case of no header is identified or is incorrect, the plain answer **``pong``** will be given.
+
+## Additional endpoints
+
+### ``/ports``
+
+If you want to check which ports your server is listning to, than use this endpoint.
+
+```bash
+curl http://localhost:13420/ports
+```
+
+```json
+["13420"]
 ```
 
 ## Configuration file
@@ -73,7 +166,7 @@ or create it manually:
 ```json
 {
     "_comment": "##########################\n\nThis is the configuration file for node-pong.\n\nports: array of ports for node-pong listen to. If no port is informed, or no node-pong.json file is found, it will listen on the default port 13420.\n\nserverMessage: should be a message that is shown on the console when the server is listening on the desired port, on the form of:\n\nserverMessage: port\n\n##########################\n",
-    "ports": ["13420", "4500", "4600-4602"],
+    "ports": ["13420"],
     "serverMessage": "The server is running on port: "
 }
 ```
@@ -98,6 +191,7 @@ Provide a simple API able to answer to HTTP requests, enabling our project to:
 - <s>Provide a default API endpoint "/ping" on a predefined default port;</s>
 - <s>Respond to ping requests on a default port;</s>
 - <s>Respond with a default answer "PONG";</s>
+- <s>Provide different type of responses other than the default plain text (HTML, JSON, XML)</s>
 - <s>Fewer dependencies as possible or no dependencies at all</s>
 - Implement in-memory list of ports, with or without file persistence, for dynamic project configuration;
 - <s>OPTIONAL: respond to ping requests on several ports;</s>
@@ -111,6 +205,11 @@ Provide a simple API able to answer to HTTP requests, enabling our project to:
 - Implement a continuous delivery process;
 
 ## Versions
+
+### 1.2.0
+
+- Added "/ports" endpoint to check which ports the server is listening to;
+- Response in multiple formats based on HTTP headers (html, json, xml)
 
 ### 1.1.1
 
--- node-pong.js
+++ node-pong.js
@@ -13,7 +13,9 @@
 
 let http = require('http');
 let fs = require('fs');
-let defaultPort = 13420;
+let urlMod = require('url');
+let portArray = ['13420'];
+let httpServers = [];
 /*load configFile from app dir, using normalize for windows/linux/mac path correction */
 let configFile = require('path').normalize(process.cwd() + '/' + 'node-pong.json');
 
@@ -36,8 +38,9 @@ if (fs.existsSync(configFile)) { //run with config file details
             let startPort = portsArray[0].trim();
             let endPort = portsArray[1].trim();
             if (isNumber(startPort) && isNumber(endPort)) { //only runs if the ports are numbers
-                for (let index = startPort; index < endPort; index++) {
+                for (let index = startPort; index <= endPort; index++) {
                     createServer(serverMessage, index);
+                    addPort('' + index); //convert number
                     startedServers++;
                 }
             } else {
@@ -47,6 +50,7 @@ if (fs.existsSync(configFile)) { //run with config file details
             let singlePort = ports[key].trim();
             if (isNumber(singlePort)) {
                 createServer(serverMessage, singlePort);
+                addPort(singlePort);
                 startedServers++;
             } else
                 console.log(singlePort + ' -> not a valid port');
@@ -62,23 +66,72 @@ if (fs.existsSync(configFile)) { //run with config file details
 }
 
 /**
- * Create a server listening on the defaultPort
+ * add port to the array of ports the server is listening to
+ * 
+ * @param {any} port 
  */
-function createDefaultServer() {
-    createServer('server running on port: ', defaultPort);
+function addPort(port) {
+    if (portArray.indexOf(port) === -1) {
+        portArray.push(port);
+    }
 }
 
 /**
- * Process the server requests
+ * Create a server listening on the defaultPort
+ */
+function createDefaultServer() {
+    //createServer('server running on port: ', defaultPort);
+    createServer('server running on port: ', portArray[0]);
+}
+
+/**
+ * Process the server requests and returns a response based on this precedence:
+ * 
+ * HTTP ACCEPT HEADERS - we want just one kind of response, so if multiple ACCEPT headers 
+ * are found, the first one on the following sequence will be the response:
+ *   - JSON
+ *   - HTML
+ *   - XML
+ * 
+ * If no header is identified or is incorrect, the plain answer "pong" will be given.
  * 
  * @param {Request} req 
  * @param {Response} res 
  */
 function processServer(req, res) {
-    if (req.url === '/ping') {
-        res.write('pong');
-        //        res.write('Response from port: ' + port);
+    const { headers, method, url } = req;
+    let parameters = urlMod.parse(req.url, true).query;
+    let host = req.headers.host;
+    let accept = req.headers.accept;
+    if (req.url.indexOf('/ping') > -1) {
+        console.log(parameters);
+        res.setHeader('Accept-Charset', 'utf-8');
+        res.setHeader('endpoint', '/ping');
+        //Check the type of response the client needs        
+        if (accept.indexOf('application/json') > -1) {
+            //valid json - https://jsonlint.com/
+            let body = '{"answer": "pong"}';
+            let responseBody = { headers, method, url, body };
+            res.setHeader('Content-Type', 'application/json');
+            res.write(JSON.stringify(responseBody));
+        } else if (accept.indexOf('text/html') > -1) {
+            //valid HTML - http://validator.w3.org
+            res.setHeader('Content-Type', 'text/html');
+            res.write('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>node-pong response</title></head><body><h1>pong</h1></body></html>');
+        } else if (accept.indexOf('application/xml') > -1) {
+            //valid XML - http://validator.w3.org
+            res.setHeader('Content-Type', 'application/xml');
+            res.write('<?xml version="1.0" encoding="utf-8" ?><answer>pong</answer>');
+        } else { //default answer if no type is specified or not defined correctly
+            res.writeHead(200, { 'Content-Type': 'text/plain' }); //writeHead must be called latest
+            res.write('pong');
+        }
         res.end();
+    }
+    if (req.url === '/ports') {
+        res.setHeader('Content-Type', 'application/json');
+        res.setHeader('endpoint', '/ports');
+        res.end(JSON.stringify(portArray));
     }
 }
 
@@ -89,7 +142,8 @@ function processServer(req, res) {
  * @param {Integer} port 
  */
 function createServer(serverMessage, port) {
-    http.createServer(processServer).listen(port);
+    let server = http.createServer(processServer).listen(port);
+    httpServers.push(server);
     console.log(serverMessage + port);
 }
 
